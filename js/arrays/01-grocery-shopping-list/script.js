5d952d1695de97689ae271e208d11664c2666c70
Zoe Mjweni <mjwenim0852@gmail.com>
2026-09-10 16:38:41 +0200

    feat: create grocery list array

---
+++ js/arrays/01-grocery-shopping-list/script.js
@@ -0,0 +1,11 @@
+// Store the grocery items in an array
+const groceryList = ["Milk", "Bread", "Eggs"];
+
+// Display the first grocery item from the array.
+console.log(groceryList[0]);
+
+// Display the second grocery item from the array.
+console.log(groceryList[1]);
+
+// Display the third grocery item from the array.
+console.log(groceryList[2]);
