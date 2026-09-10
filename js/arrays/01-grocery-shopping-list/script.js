93e6a62c46bf9ce448bb0c29fb7a91a8101f8167
Zoe Mjweni <mjwenim0852@gmail.com>
2026-09-10 22:18:35 +0200

    feat: find and remove grocery item

--- js/arrays/01-grocery-shopping-list/script.js
+++ js/arrays/01-grocery-shopping-list/script.js
@@ -1,8 +1,15 @@
 // Store the grocery items in an array
 const groceryList = ["Milk", "Bread", "Eggs"];
 
-// Add a new grocery item to the end of the array
-groceryList.push("Apples");
+// Find the position of the grocery item we want to remove
+const groceryIndex = groceryList.indexOf("Bread");
+
+// Check whether the grocery items was found
+if (groceryIndex !== -1) {
+  // Remove one grocery item from the position that was found
+  groceryList.splice(groceryIndex, 1);
+}
 
 // Display the updated grocery list
 console.log(groceryList);
+
