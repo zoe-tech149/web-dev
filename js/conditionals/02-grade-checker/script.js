a7503754a26dac892ac918fce43c5a9269e999cc
Zoe Mjweni <mjwenim0852@gmail.com>
2026-09-25 21:59:16 +0200

    feat: Clear old results  when the mark input changes

--- js/conditionals/02-grade-checker/script.js
+++ js/conditionals/02-grade-checker/script.js
@@ -4,22 +4,23 @@ const gradeInput = document.getElementById("gradeInput");
 const checkButton = document.getElementById("checkButton");
 const gradeResult = document.getElementById("gradeResult");
 
-checkButton.addEventListener("click", function(){
-
+checkButton.addEventListener("click", function () {
   const mark = gradeInput.value;
 
-  if (mark === "") {  
+  if (mark === "") {
     gradeResult.textContent = "Enter mark";
-  } else if (mark < 0 || mark > 100){ // Check for invalid mark entered
+  } else if (mark < 0 || mark > 100) {
     gradeResult.textContent = "Invalid mark entered ";
-  } else if (mark >= 80) {  
+  } else if (mark >= 80) {
     gradeResult.textContent = "Distinction";
   } else if (mark >= 50) {
     gradeResult.textContent = "Pass";
   } else {
     gradeResult.textContent = "Fail";
   }
-
 });
 
-
+// Clear old results  when the mark input changes
+gradeInput.addEventListener("input", function () {
+  gradeResult.textContent = "";
+});
