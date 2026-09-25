43256c285da6d257f49d87826ff2be1b2b2917ad
Zoe Mjweni <mjwenim0852@gmail.com>
2026-09-25 10:50:30 +0200

    feat: Checker whether a mark was entered

--- js/conditionals/02-grade-checker/script.js
+++ js/conditionals/02-grade-checker/script.js
@@ -2,17 +2,20 @@ console.log("js connected");
 
 // DOM CONNECTIONS
 
-const gradeInput = document.getElementById("gradeinput");
+const gradeInput = document.getElementById("gradeInput");
 const checkButton = document.getElementById("checkButton");
 const gradeResult = document.getElementById("gradeResult");
 
 checkButton.addEventListener("click", function(){
 
   const mark = gradeInput.value;
-
+  
+  // Check whether a mark was entered
   if (mark === "") {  
-    gradeResult.textContent = "Enter mark";
+    gradeResult.textContent ="Enter mark";
   }
+
+
 
 })
 
